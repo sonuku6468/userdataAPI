ab9159f8909f80d6042e3b4abdbaee6f125cfbda
sonuku6468 <108977415+sonuku6468@users.noreply.github.com>
2023-07-23 16:28:51 +0530

    Updates

---
+++ precache-manifest.a0c55965a44cc612c75b032805450ce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07a55471cecf7a876fa6",
+    "url": "/userdataAPI/static/css/main.7df02409.chunk.css"
+  },
+  {
+    "revision": "07a55471cecf7a876fa6",
+    "url": "/userdataAPI/static/js/main.07a55471.chunk.js"
+  },
+  {
+    "revision": "b5dec1235d8d1d118e4f",
+    "url": "/userdataAPI/static/js/1.b5dec123.chunk.js"
+  },
+  {
+    "revision": "2733fe40ec3b6e506dc0",
+    "url": "/userdataAPI/static/js/2.2733fe40.chunk.js"
+  },
+  {
+    "revision": "192ae01b90fab5c14404",
+    "url": "/userdataAPI/static/js/runtime~main.192ae01b.js"
+  },
+  {
+    "revision": "88ee0ee23cce6736815bb2acb7cae2f4",
+    "url": "/userdataAPI/index.html"
+  }
+];
